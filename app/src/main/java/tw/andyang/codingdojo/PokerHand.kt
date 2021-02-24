cf0750a97b5d4c2efb5c7faef64b30051171570d
andyang <yang.bng86@gmail.com>
2021-02-24 21:18:50 +0800

    John

---
+++ app/src/main/java/tw/andyang/codingdojo/PokerHand.kt
@@ -0,0 +1,6 @@
+package tw.andyang.codingdojo
+
+class PokerHand(private val card: Card, card2: Card, card3: Card, card4: Card, card5: Card) {
+
+    fun getType(): String = "OnePair"
+}
--- app/src/test/java/tw/andyang/codingdojo/PokerHandTest.kt
+++ app/src/test/java/tw/andyang/codingdojo/PokerHandTest.kt
@@ -1,8 +1,22 @@
 package tw.andyang.codingdojo
 
+import org.junit.Assert
 import org.junit.Test
 
 class PokerHandTest {
+    @Test
+    fun `if one pair return one pair`() {
+        //Given PokerHand
+        val ph = PokerHand(
+            Card.create("CA"),
+            Card.create("DA"),
+            Card.create("H3"),
+            Card.create("C4"),
+            Card.create("H5")
+        )
 
+        Assert.assertEquals(ph.getType(), "OnePair")
+
+    }
 
 }
