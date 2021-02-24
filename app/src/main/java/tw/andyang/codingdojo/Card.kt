0c75d2ef98ec65e21b6750b72534c5766a36304d
andyang <yang.bng86@gmail.com>
2021-02-24 20:32:41 +0800

    Yanbin

--- app/src/main/java/tw/andyang/codingdojo/Card.kt
+++ app/src/main/java/tw/andyang/codingdojo/Card.kt
@@ -1,9 +1,31 @@
 package tw.andyang.codingdojo
 
-class Card(input: String) {
+import java.lang.Exception
+import java.lang.IllegalArgumentException
 
-    val number: Int = 2
-    val suit: Suit = Suit.Heart
+class Card(val number: Int = 2, val suit: Suit = Suit.Heart) {
+
+
+
+    companion object {
+        fun create(input: String): Card {
+            val rawSuit = input[0]
+            val rawNumber = input.substring(1, input.length)
+
+            val number = try {
+                rawNumber.toInt()
+            } catch (e: Throwable){
+                when(rawNumber) {
+                    "J" -> 11
+                    "Q" -> 12
+                    "K" -> 13
+                    "A" -> 1
+                    else -> throw IllegalArgumentException("Hello rWorld")
+                }
+            }
+            return Card(number = number)
+        }
+    }
 }
 
 enum class Suit {
--- app/src/test/java/tw/andyang/codingdojo/CardTest.kt
+++ app/src/test/java/tw/andyang/codingdojo/CardTest.kt
@@ -8,9 +8,50 @@ class CardTest {
     @Test
     fun `If input is H2, suit is heart, number is 2` () {
 //        Arrange
-        var input = "H2"
-        var card = Card(input)
+        val input = "H2"
+        val card = Card.create(input)
 
         assertEquals(card.suit, Suit.Heart)
-        assertEquals(card.number, 2)    }
+        assertEquals(card.number, 2)
+    }
+
+    @Test
+    fun `If input is H3, suit is heart, number is 3` () {
+//        Arrange
+        val input = "H3"
+        val card = Card.create(input)
+
+        assertEquals(card.suit, Suit.Heart)
+        assertEquals(card.number, 3)
+    }
+
+    @Test
+    fun `If input is HJ, suit is heart, number is 11` () {
+//        Arrange
+        val input = "HJ"
+        val card = Card.create(input)
+
+        assertEquals(card.suit, Suit.Heart)
+        assertEquals(card.number, 11)
+    }
+
+    @Test
+    fun `If input is HQ, suit is heart, number is 12` () {
+//        Arrange
+        val input = "HQ"
+        val card = Card.create(input)
+
+        assertEquals(card.suit, Suit.Heart)
+        assertEquals(card.number, 12)
+    }
+
+    @Test
+    fun `If input is HK, suit is heart, number is 13` () {
+//        Arrange
+        val input = "HK"
+        val card = Card.create(input)
+
+        assertEquals(card.suit, Suit.Heart)
+        assertEquals(card.number, 13)
+    }
 }
