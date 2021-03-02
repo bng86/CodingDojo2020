6f66442c849e8133331239c3a0c7c3bbe193d8fa
andyang <yang.bng86@gmail.com>
2021-03-02 20:00:40 +0800

    Ivan

--- app/src/main/java/tw/andyang/codingdojo/Roll.kt
+++ app/src/main/java/tw/andyang/codingdojo/Roll.kt
@@ -1,5 +1,21 @@
 package tw.andyang.codingdojo
 
-class Roll(i: Int, i1: Int, i2: Int, i3: Int) {
-    fun getPoint():Int = 5
+class Roll(
+    private val point1: Int,
+    private val point2: Int,
+    private val point3: Int,
+    private val point4: Int
+) {
+
+    companion object {
+        const val SINGLE = 101
+    }
+
+    fun getPoint(): Int {
+        if (point1 == point2 && point2 == point3 && point3 == point4) {
+            return SINGLE
+        } else {
+            return 5
+        }
+    }
 }
--- app/src/test/java/tw/andyang/codingdojo/RollTest.kt
+++ app/src/test/java/tw/andyang/codingdojo/RollTest.kt
@@ -8,4 +8,9 @@ class RollTest {
     fun `roll with 1 1 2 3 get 5 point`() {
         assertEquals(5, Roll(1, 1, 2, 3).getPoint())
     }
+
+    @Test
+    fun `Roll with 1 1 1 1 get single color`() {
+        assertEquals(Roll.SINGLE, Roll(1, 1, 1, 1).getPoint())
+    }
 }
