425387c808ecc55c781233a816facf8e99691224
andyang <yang.bng86@gmail.com>
2021-03-02 21:17:09 +0800

    Yanbin

--- app/src/main/java/tw/andyang/codingdojo/Dices.kt
+++ app/src/main/java/tw/andyang/codingdojo/Dices.kt
@@ -24,21 +24,17 @@ data class Dices(
     }
 
     fun roll(): Int {
+        if (isSingle()) {
+            return getSinglePoints()
+        } else if (isLA18()) {
+            return LA18
+        } else if (isNPoint()) {
+            return getNPoint()
+        }
+
         return points.groupBy { it }.let { grouped ->
             when (grouped.size) {
-                TYPE_SINGLE -> {
-                    when (point1) {
-                        1 -> SINGLE_ONE
-                        2 -> SINGLE_TWO
-                        3 -> SINGLE_THREE
-                        4 -> SINGLE_FOUR
-                        5 -> SINGLE_FIVE
-                        6 -> SINGLE_SIX
-                        else -> {
-                            TODO()
-                        }
-                    }
-                }
+
                 TYPE_LA -> {
                     if (grouped[6]?.size == 2) {
                         LA18
@@ -82,5 +78,58 @@ data class Dices(
                 }
             }
         }
+    }
+
+    private fun getNPoint(): Int {
+        val group = points.groupBy { it }
+        return if (isTwoPair()) {
+            val max =
+                group
+                    .keys
+                    .maxByOrNull {
+                        it
+                    }
+            max!! * 2
+        } else {
+            group.values.filter { it.size ==1 }
+                .flatten()
+                .sum()
+        }
+    }
+
+    private fun isTwoPair(): Boolean {
+        val group = points.groupBy { it }
+        return (group.size == 2 && group.values.all { it.size == 2 })
+    }
+
+    private fun isNPoint(): Boolean {
+        val group = points.groupBy { it }
+        return group.size == 3 || (group.size == 2 && group.values.all { it.size == 2 })
+    }
+
+
+
+    private fun isLA18(): Boolean {
+        val group = points.groupBy { it }
+        return group.containsKey(6) && group[6]?.size == 2
+    }
+
+    private fun getSinglePoints(): Int {
+        return when (point1) {
+            1 -> SINGLE_ONE
+            2 -> SINGLE_TWO
+            3 -> SINGLE_THREE
+            4 -> SINGLE_FOUR
+            5 -> SINGLE_FIVE
+            6 -> SINGLE_SIX
+            else -> {
+                TODO()
+            }
+        }
+    }
+
+    private fun isSingle(): Boolean {
+        val group = points.groupBy { it }
+        return group.size == 1
     }
 }
